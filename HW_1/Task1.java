f599a4fe2eea89dc8a40260c63339c8117c19d2a
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2023-02-12 21:10:05 +0300

    добавлена папка с ДЗ_1

---
+++ HW_1/Task1.java
@@ -0,0 +1,20 @@
+// 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+package HW.HW_1;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.print("Введите максимальное значение: ");
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= N; i++) {
+            fact = fact * i;
+        }
+        System.out.printf("Треугольное число: %d\n", (N * (N+1))/2);
+        System.out.printf("Факториал числа %d: %d", N, fact);
+
+        sc.close();
+    }
+}
---
+++ HW_1/Task2.java
@@ -0,0 +1,8 @@
+// 2. Вывести все простые числа от 1 до 1000
+package HW.HW_1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        
+    }
+}
