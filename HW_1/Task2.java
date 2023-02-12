2a9f2b770111a709a22d9be56d4d1edd866e7c67
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2023-02-12 22:08:28 +0300

    добавлена папка с ДЗ_1 с корректировками

--- HW_1/Task2.java
+++ HW_1/Task2.java
@@ -3,17 +3,38 @@ package Java_HW.HW_1;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+// import java.util.Scanner; - для ручного ввода верхнего предела
 
 public class Task2 {
+    //реализован ручной ввод верхнего предела.
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Введите верхний предел: "); 
+    //     int input = sc.nextInt();
+    //     sc.close();
+    //     List<Integer> primes = new ArrayList<>();
+
+    //     for (int i = 2; i <= input; i++) {
+    //         boolean isPrimeNumber = true;
+
+    //         for (int j = 2; j < i; j++) {
+    //             if (i % j == 0) {
+    //                 isPrimeNumber = false;
+    //                 break;
+    //             }
+    //         }
+
+    //         if (isPrimeNumber) {
+    //             primes.add(i);
+    //         }
+    //     }
+    //     System.out.println("Простые числа: " + primes);
+    // }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Введите верхний предел: ");
-        int input = sc.nextInt();
-        sc.close();
         List<Integer> primes = new ArrayList<>();
 
-        for (int i = 2; i <= input; i++) {
+        for (int i = 2; i <= 1000; i++) {
             boolean isPrimeNumber = true;
 
             for (int j = 2; j < i; j++) {
