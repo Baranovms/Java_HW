0d7eaa3f7a7ea330da06077e43c4582d9c3e7a9d
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2023-02-19 20:06:20 +0300

    добавлена папка с ДЗ_2, задача 2

--- HW_2/Task2.java
+++ HW_2/Task2.java
@@ -5,35 +5,34 @@
 package Java_HW.HW_2;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.logging.*;
 
 public class Task2 {
-    public static void main(String[] args) throws IOException {
-        Logger logger = Logger.getLogger(Task2.class.getName());
-        FileHandler fh = new FileHandler("logsTask2.xml");
-        logger.addHandler(fh);
-        XMLFormatter xml = new XMLFormatter();
-        fh.setFormatter(xml);
-        logger.info("Sort Array");
-        int[] array = { 4, 7, 1, -8, 2, 32, 0, 15};
-        lib.printArrayInt(array);
-        int[] newArr = listSort(array);
-        System.out.printf("\n Отсортированный массив: \n");
-        lib.printArrayInt(newArr);
+    private static Logger logger = Logger.getLogger(Task2.class.getName());
+
+    public static void main(String[] args) throws IOException {    
+        int[] mass = new int[20];
+        for (int i = 0; i < mass.length; i++) {
+            Random rand = new Random();
+            mass[i] = rand.nextInt(100);
+        }
+        System.out.printf("Вывод неотсортированного массива: %s\n", Arrays.toString(mass));
+        listSort(mass);
+        System.out.printf("\nВывод отсортированного массива: %s\n", Arrays.toString(mass));
     }
 
-    public static int[] listSort(int[] arr) {
+    public static void listSort(int[] arr) {
         for (int k = 0; k < arr.length - 1; k++) {
-
-            for (int i = 0; i < arr.length - k - 1; i++) {
+            for (int i = 0; i < arr.length - k - 1; i++)
                 if (arr[i] > arr[i + 1]) {
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = temp;
+                    logger.log(Level.INFO, String.format("%d <--> %d, %s",
+                            arr[i], arr[i + 1], Arrays.toString(arr)));
                 }
-            }
         }
-        return arr;
     }
-
 }
